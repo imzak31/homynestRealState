7afef47ae1f25576ac353411c9011a97af43122c
Adrián Centeno <adriandenb@gmail.com>
2021-02-10 14:15:19 -0400

    CoffeeScript Error Fixed

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module KasazTest
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.generators.javascript_engine = :js
 
     # Configuration for the application, engines, and railties goes here.
     #
